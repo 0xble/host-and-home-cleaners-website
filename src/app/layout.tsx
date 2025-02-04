fb4b009b82093d1bb09f42984cb2f9b73a4a62d3
Brian Le <brian@brianle.xyz>
2025-02-03 19:39:07 -0800

    feat(layout): add Facebook Pixel tracking script
    - integrated Facebook Meta Pixel for tracking website visitors
    - added fallback for users with JavaScript disabled
    - included third-party scripts for additional functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,40 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang='en'>
       <body className={inter.className} suppressHydrationWarning>
+        {/* Facebook Meta Pixel - Tracks website visitors for ad targeting and conversion tracking */}
+        <Script id='fb-pixel' strategy='afterInteractive'>
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '474741372185904');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* Fallback for users with JavaScript disabled */}
+        <noscript>
+          <img
+            height='1'
+            width='1'
+            style={{ display: 'none' }}
+            src='https://www.facebook.com/tr?id=474741372185904&ev=PageView&noscript=1'
+            alt=''
+          />
+        </noscript>
+
+        {/* Main Content */}
         {children}
+
+        {/* Third-party Scripts */}
         <Script src='dist/flowbite.min.js' />
         <Script src='https://tally.so/widgets/embed.js' strategy='lazyOnload' />
+
+        {/* UI Components */}
         <Toaster />
       </body>
     </html>
